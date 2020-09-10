5a8fbe90cfe803e94710c5a51edb03035178f5c1
jessenwells <jessenwells@gmail.com>
2020-09-10 17:41:08 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 import { registerRootComponent } from 'expo'
 import App from './src/App'
 
-//dev
-import { YellowBox } from 'react-native'
-YellowBox.ignoreWarnings(['Setting a timer', 'Remote debugger'])
-
 registerRootComponent(App)
